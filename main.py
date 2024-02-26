5fecf237f98a1fbcd3a84f5d8e49a96dea241c75
DmitryAce <Dm1tryAce@yandex.ru>
2024-02-26 18:55:07 +0300

    Huge update
    + Фиск границ карты
    + Сюжет с монетами
    + Выходы

--- main.py
+++ main.py
@@ -1,5 +1,34 @@
-import random
 import time
+
+
+def xorshift32(x):
+    x ^= x << 13
+    x ^= x >> 17
+    x ^= x << 5
+    return x & 0xffffffff
+
+
+class Rand:
+    def __init__(self, seed, algo=xorshift32, rand_max=0xffffffff):
+        self.algo = algo
+        self.rand_max = rand_max
+        self.seed = seed
+
+    def random(self):
+        self.seed = self.algo(self.seed)
+        return self.seed
+
+    def randint(self, low, high):
+        return low + self.random() % (high - low + 1)
+
+    def choice(self, seq):
+        return seq[self.randint(0, len(seq) - 1)]
+
+    def randf(self):
+        return self.random() / self.rand_max
+
+    def uniform(self, low, high):
+        return low + (high - low) * self.randf()
 
 
 def save_map(game_map, filename):
@@ -9,7 +38,7 @@ def save_map(game_map, filename):
             f.write('"' + ''.join(row) + '",\n')
 
 
-def gen_board(width, height, rooms):
+def gen_board_with_caves(width, height, rooms):
     """Генератор начальной карты с пещерами"""
     # Создаем пустую карту
     game_map = [['B' for _ in range(width)] for _ in range(height)]
@@ -76,25 +105,41 @@ def interp(board, init, rules):
                 break
 
 
-def addcoins(board, height, width, coin_chance):
-    for y in range(height):
-        for x in range(width):
-            if board[y][x] == ' ' and random.random() < coin_chance:
-                board[y][x] = '1'
-    return board
+def addwalls(board, sizex, sizey):
+    # Верхняя граница
+    for x in range(0, sizex):
+        if board[0][x] != "#":
+            board.insert(0, ["#"] * (sizex))
+            sizey += 1
+            break
+
+    # Нижняя граница
+    for x in range(0, sizex):
+        if board[sizey-1][x] != "#":
+            board.append(["#"] * (sizex))
+            sizey += 1
+            break
+
+    # левая граница
+    for y in range(0, sizey):
+        if board[y][0] != "#":
+            for x in range(0, sizey):
+                board[x].insert(0, "#")
+            sizex += 1
+            break
+
+    # Правая граница
+    for y in range(0, sizey):
+        if board[y][-1] != "#":
+            for x in range(0, sizey):
+                board[x].append("#")
+            sizex += 1
+            break
+
+    return board, sizex, sizey
 
 
-def addwalls1010(board, wdth):
-    # Добавляем верхнюю и нижнюю стены
-    board.insert(0, ["#"] * wdth)
-    board.append(["#"] * wdth)
-
-    return board
-
-
-def main(seed):
-    random.seed(seed)
-
+def main():
     events = ["coins", "exit", "treasure"]
     gen_method = ["MazeBacktracker", "MazeGrowth", "NoDeadEnds"]
     N = random.randint(1, 3)
@@ -107,7 +152,7 @@ def main(seed):
     height = width
 
     filename = 'board.txt'  # Имя файла для сохранения карты
-    coin_chance = random.uniform(0.1, 0.2)
+    coin_chance = random.uniform(0.4, 0.7)
 
     board = [['B' for _ in range(width)] for _ in range(height)]
 
@@ -143,12 +188,14 @@ def main(seed):
                 x] == 'R' or board[y][x] == 'A':
                 board[y][x] = ' '
 
+    board, width, height = addwalls(board, width, height)
+
     # ---TILES---
     """Сделал чтобы можно было проверить определьные точки 
     при создании сокровищ"""
     tiles = []
-    for y in range(len(board)):
-        for x in range(len(board[y])):
+    for y in range(1, len(board) - 1):
+        for x in range(1, len(board[y]) - 1):
             tiles.append([x, y])
 
     # ---PLOT---
@@ -161,6 +208,8 @@ def main(seed):
     print(plot, " - ", N, ", ", gen_method)
 
     # --TREASURES--
+    # Создаем комнаты
+    treasure_coordinates = []
     if "treasure" in plot:
         rooms = random.randint(1, 3)
 
@@ -174,9 +223,6 @@ def main(seed):
         task.append(
             f"Необходимо найти {format_treasures(rooms)}, чтобы собирать " + \
             "сокровища нужно подбирать ключи.")
-
-        # Создаем комнаты
-        treasure_coordinates = []
 
         while rooms:
             x = random.randint(2, width - 3)  # Координаты сокровища
@@ -205,8 +251,7 @@ def main(seed):
                 x -= 1
             elif x == width - 4:
                 x += 1
-            # +2 к размеру Из-за добавления верхней и нижней стены
-            if y == height - 4 + 2:
+            if y == height - 4:
                 y += 1
             elif y == 3:
                 y -= 1
@@ -250,20 +295,25 @@ def main(seed):
                 # Расположение двери
                 door = random.randint(1, 4)
 
+                # Выбор стороны для двери и проверка что вход не находится на краю карты (внутри блока)
                 if door == 1 and y != 2:
                     k = random.randint(1, 3)
+                    if not ([x - 2 + k, y - 2] in tiles): continue
                     board[y - 2][x - 2 + k] = 'd'
                     break
                 elif door == 2 and x != width - 3:
                     k = random.randint(1, 3)
+                    if not ([x + 2, y - 2 + k] in tiles): continue
                     board[y - 2 + k][x + 2] = 'd'
                     break
                 elif door == 3 and y != height - 3:
                     k = random.randint(1, 3)
+                    if not ([x - 2 + k, y + 2] in tiles): continue
                     board[y + 2][x - 2 + k] = 'd'
                     break
                 elif door == 2 and x != 2:
                     k = random.randint(1, 3)
+                    if not ([x - 2, y - 2 + k] in tiles): continue
                     board[y - 2 + k][x - 2] = 'd'
                     break
 
@@ -279,13 +329,92 @@ def main(seed):
                         board[j][i] = "k"
                         break
 
+    # --COINS--
+    if "coins" in plot:
+        coins_goal = random.randint(10, 40)
+        task.append(
+            f"Необходимо собрать {coins_goal} *.")
+
+        # Увеличиваем кол-во монет на карте, чтобы боло с запасом
+        coins_goal += int(coin_chance * coins_goal)
+
+        while coins_goal:
+            x = random.randint(0, width-1)
+            y = random.randint(0, height-1)
+
+            if board[y][x] == " ":
+                board[y][x] = "1"
+                coins_goal -= 1
+
     # --EXIT--
-    ''' Для сюжета с выходом необходимо будет доработать добавление на карту недостающих сплошных стен'''
+    if "exit" in plot:
+        # Случайное количество выходов на карте
+        exits = random.randint(1,3)
+
+        while exits:
+            direction = random.randint(1,4)
+
+            # Много проверок учитывают чтобы выход не появился внутри сокровищницы
+            match direction:
+                case 1:
+                    x = random.randint(1,width-2)
+                    if board[0][x] == "#" and board[1][x] != "#":
+                        flag = True
+                        for center in treasure_coordinates:
+                            if (center[0]-2 < x < center[0]+2) and (center[1] == 2 or center[1] == 3):
+                                flag = False
+                                break
+                    if not flag: continue
+                    else:
+                        board[0][x] = "E"
+                        exits -= 1
+                case 2:
+                    y = random.randint(1, height - 2)
+                    if board[y][-1] == "#" and board[y][-2] != "#":
+                        flag = True
+                        for center in treasure_coordinates:
+                            if (center[0] > width-4) and (center[1]-2 < y < center[1]+2):
+                                flag = False
+                                break
+                    if not flag:
+                        continue
+                    else:
+                        board[y][-1] = "E"
+                        exits -= 1
+                case 3:
+                    x = random.randint(1, width - 2)
+                    if board[-1][x] == "#" and board[-2][x] != "#":
+                        flag = True
+                        for center in treasure_coordinates:
+                            if (center[0] - 2 < x < center[0] + 2) and (center[1] == height - 2 or center[1] == height - 3):
+                                flag = False
+                                break
+                    if not flag:
+                        continue
+                    else:
+                        board[-1][x] = "E"
+                        exits -= 1
+                case 4:
+                    y = random.randint(1, height - 2)
+                    if board[y][0] == "#" and board[y][1] != "#":
+                        flag = True
+                        for center in treasure_coordinates:
+                            if (center[0] < 4) and (center[1] - 2 < y < center[1] + 2):
+                                flag = False
+                                break
+                    if not flag:
+                        continue
+                    else:
+                        board[y][0] = "E"
+                        exits -= 1
+
+        task.append(
+            f"Escape from DandyBot.")
 
     print(task)
-    addcoins(board, height, width, coin_chance)
     save_map(board, filename)
 
 
 if __name__ == '__main__':
-    main(157)
+    random = Rand(252151)
+    main()
